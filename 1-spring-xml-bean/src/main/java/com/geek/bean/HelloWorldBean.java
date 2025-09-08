deefff020a8281ebe00e46030f455148dc0739d7
vidhijain <vidhi.jain@nl.abnamro.com>
2025-09-08 13:50:49 +0200

    create beans via xml

---
+++ 1-spring-xml-bean/src/main/java/com/geek/bean/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.geek.bean;
+
+public class HelloWorldBean {
+    private String msg;
+    public void setMsg(String msg) {
+        this.msg = msg;
+        System.out.println("setter");
+    }
+
+    public HelloWorldBean(String msg,String msg2) {
+        System.out.println("constructor" +msg2);
+        this.msg=msg;
+    }
+    public String sayHello(String name) {
+        return "Hi "+name+" "+msg;
+    }
+}
+
